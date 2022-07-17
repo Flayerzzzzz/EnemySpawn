0d4e77c77b20ecbdb665193c23ca92f4eecc9c09
Flayerzzzzz <ND_02@list.ru>
2022-07-18 00:14:35 +0300

    1

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemy;
+    [SerializeField] private Enemy _enemy;
     [SerializeField] private Transform _spawnPoints;
-    [SerializeField] private float _timeBetweeòSpawn;
+    [SerializeField] private float _timeBetweenSpawn;
 
     private Transform[] _points;
-    private float _timeToSpawn;
-    private int _numberOfSpawn;
     
 
     public void Start()
     {
-        _timeToSpawn = _timeBetweeòSpawn;
         _points = new Transform[_spawnPoints.childCount];
-        _numberOfSpawn = 0;
 
         for (int i = 0; i < _spawnPoints.childCount; i++)
         {
             _points[i] = _spawnPoints.GetChild(i);
         }
+
+        StartCoroutine(EnemySpawn());
     }
 
-    public void Update()
+    private IEnumerator EnemySpawn()
     {
-        if (_timeToSpawn <= 0)
+        for (int i = 0; i <= _points.Length; i++)
         {
-            Instantiate(_enemy, _points[_numberOfSpawn].position, Quaternion.identity);
-            _timeToSpawn = _timeBetweeòSpawn;
-            _numberOfSpawn++;
+            Instantiate(_enemy, _points[i].position, Quaternion.identity);
 
-            if (_numberOfSpawn == _points.Length)
+            if (i == _points.Length - 1)
             {
-                _numberOfSpawn = 0;
+                i = -1;
             }
-        }
-        else
-        {
-            _timeToSpawn -= Time.deltaTime;
+            yield return new WaitForSeconds(_timeBetweenSpawn);
         }
     }
 }
